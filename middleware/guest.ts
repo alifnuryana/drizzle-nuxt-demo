eb8d286aa6234c78ac68ee06aec9ebc5e5362104
Alif Nuryana <nuryana.alif@hotmail.com>
2024-09-27 18:05:04 +0700

    Update guest.ts

--- middleware/guest.ts
+++ middleware/guest.ts
@@ -1,8 +1,4 @@
 export default defineNuxtRouteMiddleware((to, _from) => {
-  if (to.path === "/login" || to.path === "/register") {
-    return;
-  }
-
   const { loggedIn } = useUserSession();
 
   if (loggedIn.value) {
